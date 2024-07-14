d6c53fa3365f293e3902891c81350599ad515516
sagarsanidhya <sagarsanidhya@gmail.com>
2024-07-14 21:27:14 +0530

    Activity 3, 4,5 done

---
+++ Day 2 Operators/Activity4_logical.js
@@ -0,0 +1,12 @@
+//Task 11
+let x =5, y =10, z =15;
+console.log(x>y && x>z);
+console.log(z>x && z>y);
+
+//Task 12
+let a = 10, b = 15, c = 20;
+console.log(a>b || a>c);
+console.log(c>b || c>a);
+
+//Task 13
+console.log(x=!a);
---
+++ Day 2 Operators/Activity5_ternary.js
@@ -0,0 +1,4 @@
+//Task 14
+let number = -5;
+let result = number >= 0 ? "Positive" : "Negative"; // it works as an options 
+console.log(result);
