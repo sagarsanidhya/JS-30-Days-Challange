9e704847e3fb48078ce7a288f7517808d5de64bc
sagarsanidhya <sagarsanidhya@gmail.com>
2024-07-14 21:07:29 +0530

    activity 3 done

---
+++ Day 2 Operators/Activity3_comparison.js
@@ -0,0 +1,16 @@
+//Task 8
+let x = 6, y =7;
+console.log(x>y);
+console.log(x<y);
+
+//Task 9
+let a= 6, b= 7;
+console.log(a>=b);
+console.log(a<=b);
+
+//Task 10
+let m = 6, n= 6;
+console.log(m==n);
+
+let l = 6, j='6';
+console.log(l===j); // as === also compare the type of values in comparison
