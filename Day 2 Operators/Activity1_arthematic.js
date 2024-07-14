fc40ec8cf02647a08739ceedd575f756083b3032
sagarsanidhya <sagarsanidhya@gmail.com>
2024-07-14 20:53:18 +0530

    Activity 1 done

---
+++ Day 2 Operators/Activity1_arthematic.js
@@ -0,0 +1,22 @@
+//Task 1
+let x = 5 , y = 10;
+let z =  x + y;
+console.log("The sum is :",z);
+
+//Task 2
+let a = 9 , b = 5 ;
+let c = a-b ;
+console.log("The Difference is :",c);
+
+//Task 3
+let no1 = 10 , no2 =10;
+let product = no1 * no2;
+console.log("The product is :",product);
+
+//Task 4
+let quotient = y/x;
+console.log("The quotient is :",quotient);
+
+//Task 5
+let remainder = y%x;
+console.log("The remainder is :",remainder);
